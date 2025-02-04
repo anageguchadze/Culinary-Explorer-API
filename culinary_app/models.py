dc9f3f865c8b98621e57ea8ee266da3ecad18943
Ana Geguchadze <anageguchadze22@gmail.com>
2025-02-04 15:28:26 +0100

    Add rating min and max values

--- culinary_app/models.py
+++ culinary_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Chef(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -29,7 +30,12 @@ class Dish(models.Model):
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     dish = models.ForeignKey(Dish, on_delete=models.CASCADE, related_name='ratings')
-    score = models.IntegerField()
+    score = models.IntegerField(
+        validators=[
+            MinValueValidator(1),  
+            MaxValueValidator(5)  
+        ]
+    )
 
     def __str__(self):
         return f"{self.user.username} - {self.dish.name} - {self.score}"
